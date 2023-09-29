e9cfcd2c5059cddd3758c95fdba14f3b7ddbb713
GitHub Action <action@github.com>
2023-09-29 16:03:04 +0000

    Prepend comments to source files

--- mobile/src/screens/ExpiredPaymentRequests.tsx
+++ mobile/src/screens/ExpiredPaymentRequests.tsx
@@ -1,3 +1,7 @@
+// Copyright 2023 Quantoz Technology B.V. and contributors. Licensed
+// under the Apache License, Version 2.0. See the NOTICE file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
 import { MaterialTopTabScreenProps } from "@react-navigation/material-top-tabs";
 import { PaymentRequestsTabParamList } from "../navigation/PaymentRequestsTab";
 import PaymentRequestsList from "../components/PaymentRequestsList";
