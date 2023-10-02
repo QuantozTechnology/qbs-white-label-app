39e0fd52ea07f1f15645c3f56d5826ea762de88a
GitHub Action <action@github.com>
2023-10-02 08:24:19 +0000

    Prepend comments to source files

--- mobile/src/screens/__tests__/SecurityCode.test.tsx
+++ mobile/src/screens/__tests__/SecurityCode.test.tsx
@@ -1,3 +1,7 @@
+// Copyright 2023 Quantoz Technology B.V. and contributors. Licensed
+// under the Apache License, Version 2.0. See the NOTICE file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
 jest.mock("totp-generator", () => jest.fn(() => "123456"));
 jest.mock("expo-secure-store", () => ({
   isAvailableAsync: jest.fn(() => Promise.resolve(true)),
