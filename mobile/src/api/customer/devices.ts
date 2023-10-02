4cc1ef21f6c829dcb6cdc833fd22ca270b05196c
Alessandro Capra <alessandro@quantoz.com>
2023-10-02 11:42:45 +0200

    Try real api

--- mobile/src/api/customer/devices.ts
+++ mobile/src/api/customer/devices.ts
@@ -3,16 +3,16 @@
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 import { AxiosResponse } from "axios";
-import { mockPaymentsApi } from "../../utils/axios";
+import { paymentsApi } from "../../utils/axios";
 import { Device, DevicesPayload } from "./devices.interface";
 import { GenericApiResponse } from "../utils/api.interface";
 
 export function verifyDevice(
   payload: DevicesPayload
 ): Promise<AxiosResponse<GenericApiResponse<Device>, DevicesPayload>> {
-  return mockPaymentsApi.post("/api/customers/devices", payload, {
-    headers: {
-      "x-mock-response-code": 409,
-    },
+  return paymentsApi.post("/api/customers/devices", payload, {
+    // headers: {
+    //   "x-mock-response-code": 200,
+    // },
   });
 }
