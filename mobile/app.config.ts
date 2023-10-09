dbbbc90009122f70e248ba88e7ec732b85b12539
Alessandro Capra <alessandro@quantoz.com>
2023-10-09 16:51:07 +0200

    Try renaming env vars for build

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -11,17 +11,17 @@
 // - Expo Secrets (for build processes)
 
 const AppConfig = {
-  name: process.env.EXPO_PUBLIC_APP_NAME,
+  name: process.env.APP_NAME,
   icon: "./assets/icon-qbs.png",
   image: "./assets/splash-qbs.png",
   ios: {
-    bundleIdentifier: process.env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER,
+    bundleIdentifier: process.env.IOS_BUNDLE_IDENTIFIER,
   },
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon-qbs.png",
     },
-    package: process.env.EXPO_PUBLIC_ANDROID_PACKAGE_NAME,
+    package: process.env.ANDROID_PACKAGE_NAME,
   },
   // This logic sets the correct env variables to use throughout the app
   // They can be read through the `expo-constants` package (https://docs.expo.dev/guides/environment-variables/#reading-environment-variables)
@@ -103,7 +103,7 @@ export default () => ({
         },
       ],
     ],
-    scheme: process.env.EXPO_PUBLIC_SCHEME,
+    scheme: process.env.SCHEME,
     platforms: ["ios", "android"],
     runtimeVersion: {
       policy: "sdkVersion",
