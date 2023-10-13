de12325436e7cd76a9b8d13d7020eb8550a02cc3
neha@quantoz <neha@quantoz.com>
2023-10-13 13:38:52 +0200

    Added Signature verification middleware

---
+++ Core.APITests/ResponseHandlingTests/SignatureVerificationMiddlewareTests.cs
@@ -0,0 +1,164 @@
+﻿using Core.API.ResponseHandling;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.APITests.ResponseHandlingTests
+{
+    [TestClass()]
+    public class SignatureVerificationMiddlewareTests
+    {
+        [TestMethod]
+        public async Task Middleware_ValidSignature_ContinuesWithRequest()
+        {
+            // Arrange
+            var publicKey = "-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnLQKInx7UeHsc99RPHHT\r\ndBs5jQZoTOswJc6pE4+dggmbf4b/XKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHz\r\nd2ZPjyeakERGjS2ZKBCo1ysYnP0bROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s\r\n3hVLOzdLlHEisjTkHUsDJATg8J8EHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko\r\n+bVQxtv1/ZiGhDsWUPNGss1hXmXBQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZ\r\nwRvezSnUfGcY0h/HG/TMs1EGfUecgI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+f\r\ngwIDAQAB\r\n-----END PUBLIC KEY-----";
+            var privateKey = "-----BEGIN RSA PRIVATE KEY-----\r\nMIIEpAIBAAKCAQEAnLQKInx7UeHsc99RPHHTdBs5jQZoTOswJc6pE4+dggmbf4b/\r\nXKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHzd2ZPjyeakERGjS2ZKBCo1ysYnP0b\r\nROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s3hVLOzdLlHEisjTkHUsDJATg8J8E\r\nHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko+bVQxtv1/ZiGhDsWUPNGss1hXmXB\r\nQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZwRvezSnUfGcY0h/HG/TMs1EGfUec\r\ngI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+fgwIDAQABAoIBAQCXZIl2D/XEghTD\r\nTblaQE4eGj9btBkIVyBJJoPK1jFB9K46Yt5LS2I/ie8VnBgEcpVa1FCJ5tBha14V\r\njMTG5S7m5/1tPMHjJ1NSCf+x6YZ1erlo0k+KHldaBsdjk9iRwT9Uh+kBGeMUt78C\r\nIKbpdXYmiUQJjb6DR1pR+S957YK3REro6HWBhYwRAnPCukchaD6efaUN2yoqm/7g\r\nMy5avNFeJ+3VaR+RejylZd+IoGIAYRW7Lgu5x2g9SD1O2HaX/tfPj9ouz+5c8J2e\r\nciNnDqI8M78zhgpcFvgoFdHNL+UiSUEGen+ZCT9JiPNEH/AI2zxc8TtCjIpA0qJk\r\nJ/MxRP6BAoGBANpd9+wC/meOcsOPBZYra/vIc7Oo2OVOrdYZQQ89so2ZlbtSoPP6\r\n9wN84d6M56oulApE4F4zdlgojSWo3S/4qN93ZptR1vuSIml7eAkH9BQJSnlHgFjf\r\ntdF1zcXV4bSxnqkdZh5i77aioVm0fCkdJgQyRbewiqp7Jp/z5dqpseYRAoGBALe1\r\njmeXZ3bIzbq33EO+pc3NIQODhpDiRsZJfrneH7tm4N2UWp8vBKBqjwmMxFO4LMPx\r\nH0amnhtLvq0cOCgVyN9zekTWtIuyOw9KKfrmJ3zfRY1oRV2Fn+h6dWIveWj1hm0H\r\n6yv5xznaUuNzlRubXshWyC/eyMPAUatYrf5tKYhTAoGAMt5/GcDcyPz7KSlRMNlu\r\nr1nT8j9cP5bjkiOR7139EVV89wVZr1yAXJSj/XcvpIpzPC0tY2RzpjfUIbjDxiAU\r\nHvKuuXIINdSmJZJ4tQngRyae7b/FW27J6UCbLgIUMUbLYjQSDPQZSZ97HO2Zmu5K\r\nY+HeMdtzgiFsLwjfO+AaLDECgYAO9SFbHeC2szLM+RteCK/HSeRePN8//Kx2iJVg\r\n3M0InR/B6spWG6XsycBLrsJtbpl2erNpNTe6UTh9L8cCvINWbjiOUkzw8toMLKWu\r\nX/7nE+a91LeRHcgfTZkxHVxtR1BioDptojCubTBChK6nSMc22JoEC8ec6JO9t8Ky\r\n7IBtMQKBgQDWIihg/WYVZh6a7LQ2LayHJ39JvOUhOPUK0LN3hxlUnIiJWczJf41i\r\nt8c/ooKmrFMGh3Nc9P42wqdsRQrnt1de3otGNQmAgrO65QZbOvr7hhSp6znb6d+T\r\nytUeiYdFC/xmBbQoZ6Fz3r72T36VWuRatULkYrOAGTKlwtWKD3oByQ==\r\n-----END RSA PRIVATE KEY-----";
+            string? postPayload = string.Empty;
+
+            long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            currentTimestamp = (currentTimestamp / 30) * 30; // Round to the nearest 30 seconds
+
+            var publicKeyHeader = publicKey; 
+
+            var payload = new
+            {
+                publicKey,
+                timestamp = currentTimestamp.ToString(),
+                postPayload
+            };
+
+            var payloadJson = JsonConvert.SerializeObject(payload, Formatting.None);
+            var payloadBytes = Encoding.UTF8.GetBytes(payloadJson);
+
+            var signature = SignPayload(privateKey, payloadBytes);
+            var signatureHeader = Convert.ToBase64String(signature);
+
+            var logger = new Mock<ILogger<SignatureVerificationMiddleware>>();
+            var context = new DefaultHttpContext();
+
+            context.Request.Headers["x-public-key"] = publicKeyHeader;
+            context.Request.Headers["x-signature"] = signatureHeader;
+
+            var requestStream = new MemoryStream(Encoding.UTF8.GetBytes(payloadJson));
+            context.Request.Body = requestStream;
+            context.Request.Method = "GET";
+
+            var next = new RequestDelegate(innerContext => Task.CompletedTask);
+            var middleware = new SignatureVerificationMiddleware(next, logger.Object);
+
+            // Act
+            await middleware.Invoke(context);
+
+            // Assert
+            // Ensure that the middleware allowed the request to continue
+            Assert.AreEqual(200, context.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Middleware_ValidSignature_WithPostPayload_ContinuesWithRequest()
+        {
+            // Arrange
+            var publicKeyHeader = "-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnLQKInx7UeHsc99RPHHT\r\ndBs5jQZoTOswJc6pE4+dggmbf4b/XKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHz\r\nd2ZPjyeakERGjS2ZKBCo1ysYnP0bROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s\r\n3hVLOzdLlHEisjTkHUsDJATg8J8EHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko\r\n+bVQxtv1/ZiGhDsWUPNGss1hXmXBQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZ\r\nwRvezSnUfGcY0h/HG/TMs1EGfUecgI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+f\r\ngwIDAQAB\r\n-----END PUBLIC KEY-----";
+            var privateKey = "-----BEGIN RSA PRIVATE KEY-----\r\nMIIEpAIBAAKCAQEAnLQKInx7UeHsc99RPHHTdBs5jQZoTOswJc6pE4+dggmbf4b/\r\nXKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHzd2ZPjyeakERGjS2ZKBCo1ysYnP0b\r\nROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s3hVLOzdLlHEisjTkHUsDJATg8J8E\r\nHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko+bVQxtv1/ZiGhDsWUPNGss1hXmXB\r\nQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZwRvezSnUfGcY0h/HG/TMs1EGfUec\r\ngI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+fgwIDAQABAoIBAQCXZIl2D/XEghTD\r\nTblaQE4eGj9btBkIVyBJJoPK1jFB9K46Yt5LS2I/ie8VnBgEcpVa1FCJ5tBha14V\r\njMTG5S7m5/1tPMHjJ1NSCf+x6YZ1erlo0k+KHldaBsdjk9iRwT9Uh+kBGeMUt78C\r\nIKbpdXYmiUQJjb6DR1pR+S957YK3REro6HWBhYwRAnPCukchaD6efaUN2yoqm/7g\r\nMy5avNFeJ+3VaR+RejylZd+IoGIAYRW7Lgu5x2g9SD1O2HaX/tfPj9ouz+5c8J2e\r\nciNnDqI8M78zhgpcFvgoFdHNL+UiSUEGen+ZCT9JiPNEH/AI2zxc8TtCjIpA0qJk\r\nJ/MxRP6BAoGBANpd9+wC/meOcsOPBZYra/vIc7Oo2OVOrdYZQQ89so2ZlbtSoPP6\r\n9wN84d6M56oulApE4F4zdlgojSWo3S/4qN93ZptR1vuSIml7eAkH9BQJSnlHgFjf\r\ntdF1zcXV4bSxnqkdZh5i77aioVm0fCkdJgQyRbewiqp7Jp/z5dqpseYRAoGBALe1\r\njmeXZ3bIzbq33EO+pc3NIQODhpDiRsZJfrneH7tm4N2UWp8vBKBqjwmMxFO4LMPx\r\nH0amnhtLvq0cOCgVyN9zekTWtIuyOw9KKfrmJ3zfRY1oRV2Fn+h6dWIveWj1hm0H\r\n6yv5xznaUuNzlRubXshWyC/eyMPAUatYrf5tKYhTAoGAMt5/GcDcyPz7KSlRMNlu\r\nr1nT8j9cP5bjkiOR7139EVV89wVZr1yAXJSj/XcvpIpzPC0tY2RzpjfUIbjDxiAU\r\nHvKuuXIINdSmJZJ4tQngRyae7b/FW27J6UCbLgIUMUbLYjQSDPQZSZ97HO2Zmu5K\r\nY+HeMdtzgiFsLwjfO+AaLDECgYAO9SFbHeC2szLM+RteCK/HSeRePN8//Kx2iJVg\r\n3M0InR/B6spWG6XsycBLrsJtbpl2erNpNTe6UTh9L8cCvINWbjiOUkzw8toMLKWu\r\nX/7nE+a91LeRHcgfTZkxHVxtR1BioDptojCubTBChK6nSMc22JoEC8ec6JO9t8Ky\r\n7IBtMQKBgQDWIihg/WYVZh6a7LQ2LayHJ39JvOUhOPUK0LN3hxlUnIiJWczJf41i\r\nt8c/ooKmrFMGh3Nc9P42wqdsRQrnt1de3otGNQmAgrO65QZbOvr7hhSp6znb6d+T\r\nytUeiYdFC/xmBbQoZ6Fz3r72T36VWuRatULkYrOAGTKlwtWKD3oByQ==\r\n-----END RSA PRIVATE KEY-----";
+            string postPayload = "{\"publicKey\": \"" + publicKeyHeader + "\", \"otpcode\": \"123456\"}";
+
+            long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            currentTimestamp = (currentTimestamp / 30) * 30; // Round to the nearest 30 seconds
+
+            var payload = new
+            {
+                publicKey = publicKeyHeader,
+                timestamp = currentTimestamp.ToString(),
+                postPayload
+            };
+
+            var payloadJson = JsonConvert.SerializeObject(payload, Formatting.None);
+            var payloadBytes = Encoding.UTF8.GetBytes(payloadJson);
+
+            // Perform the signature calculation using the appropriate private key
+            var signature = SignPayload(privateKey, payloadBytes);
+            var signatureHeader = Convert.ToBase64String(signature);
+
+            var logger = new Mock<ILogger<SignatureVerificationMiddleware>>();
+            var context = new DefaultHttpContext();
+
+            context.Request.Headers["x-public-key"] = publicKeyHeader;
+            context.Request.Headers["x-signature"] = signatureHeader;
+
+            var requestStream = new MemoryStream(Encoding.UTF8.GetBytes(postPayload));
+            context.Request.Body = requestStream;
+            context.Request.Method = "POST";
+
+            var next = new RequestDelegate(innerContext => Task.CompletedTask);
+            var middleware = new SignatureVerificationMiddleware(next, logger.Object);
+
+            // Act
+            await middleware.Invoke(context);
+
+            // Assert
+            Assert.AreEqual(200, context.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Middleware_InvalidSignature_ReturnsForbidden()
+        {
+            // Arrange
+            var publicKeyHeader = "-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnLQKInx7UeHsc99RPHHT\r\ndBs5jQZoTOswJc6pE4+dggmbf4b/XKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHz\r\nd2ZPjyeakERGjS2ZKBCo1ysYnP0bROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s\r\n3hVLOzdLlHEisjTkHUsDJATg8J8EHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko\r\n+bVQxtv1/ZiGhDsWUPNGss1hXmXBQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZ\r\nwRvezSnUfGcY0h/HG/TMs1EGfUecgI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+f\r\ngwIDAQAB\r\n-----END PUBLIC KEY-----";
+            var privateKey = "-----BEGIN RSA PRIVATE KEY-----\r\nMIIEpAIBAAKCAQEAnLQKInx7UeHsc99RPHHTdBs5jQZoTOswJc6pE4+dggmbf4b/\r\nXKDxNrKCpHVXMKRrotGdHKv9xOMhJFHPyZHzd2ZPjyeakERGjS2ZKBCo1ysYnP0b\r\nROdoNAEbAIOyMutiTkYEuf/a7Rr5ptJQ323s3hVLOzdLlHEisjTkHUsDJATg8J8E\r\nHx/pRJD48+Q0LHkArhmbIKol4lDRe2ODfgko+bVQxtv1/ZiGhDsWUPNGss1hXmXB\r\nQCzx4VXgi6DfeTkn/GyyaonumVEkaR+KUKxZwRvezSnUfGcY0h/HG/TMs1EGfUec\r\ngI6bCsIv6lD5U4AMkHvt4e+dckRgSV5cnJ+fgwIDAQABAoIBAQCXZIl2D/XEghTD\r\nTblaQE4eGj9btBkIVyBJJoPK1jFB9K46Yt5LS2I/ie8VnBgEcpVa1FCJ5tBha14V\r\njMTG5S7m5/1tPMHjJ1NSCf+x6YZ1erlo0k+KHldaBsdjk9iRwT9Uh+kBGeMUt78C\r\nIKbpdXYmiUQJjb6DR1pR+S957YK3REro6HWBhYwRAnPCukchaD6efaUN2yoqm/7g\r\nMy5avNFeJ+3VaR+RejylZd+IoGIAYRW7Lgu5x2g9SD1O2HaX/tfPj9ouz+5c8J2e\r\nciNnDqI8M78zhgpcFvgoFdHNL+UiSUEGen+ZCT9JiPNEH/AI2zxc8TtCjIpA0qJk\r\nJ/MxRP6BAoGBANpd9+wC/meOcsOPBZYra/vIc7Oo2OVOrdYZQQ89so2ZlbtSoPP6\r\n9wN84d6M56oulApE4F4zdlgojSWo3S/4qN93ZptR1vuSIml7eAkH9BQJSnlHgFjf\r\ntdF1zcXV4bSxnqkdZh5i77aioVm0fCkdJgQyRbewiqp7Jp/z5dqpseYRAoGBALe1\r\njmeXZ3bIzbq33EO+pc3NIQODhpDiRsZJfrneH7tm4N2UWp8vBKBqjwmMxFO4LMPx\r\nH0amnhtLvq0cOCgVyN9zekTWtIuyOw9KKfrmJ3zfRY1oRV2Fn+h6dWIveWj1hm0H\r\n6yv5xznaUuNzlRubXshWyC/eyMPAUatYrf5tKYhTAoGAMt5/GcDcyPz7KSlRMNlu\r\nr1nT8j9cP5bjkiOR7139EVV89wVZr1yAXJSj/XcvpIpzPC0tY2RzpjfUIbjDxiAU\r\nHvKuuXIINdSmJZJ4tQngRyae7b/FW27J6UCbLgIUMUbLYjQSDPQZSZ97HO2Zmu5K\r\nY+HeMdtzgiFsLwjfO+AaLDECgYAO9SFbHeC2szLM+RteCK/HSeRePN8//Kx2iJVg\r\n3M0InR/B6spWG6XsycBLrsJtbpl2erNpNTe6UTh9L8cCvINWbjiOUkzw8toMLKWu\r\nX/7nE+a91LeRHcgfTZkxHVxtR1BioDptojCubTBChK6nSMc22JoEC8ec6JO9t8Ky\r\n7IBtMQKBgQDWIihg/WYVZh6a7LQ2LayHJ39JvOUhOPUK0LN3hxlUnIiJWczJf41i\r\nt8c/ooKmrFMGh3Nc9P42wqdsRQrnt1de3otGNQmAgrO65QZbOvr7hhSp6znb6d+T\r\nytUeiYdFC/xmBbQoZ6Fz3r72T36VWuRatULkYrOAGTKlwtWKD3oByQ==\r\n-----END RSA PRIVATE KEY-----";
+            var validPostPayload = "{\"publicKey\": \"" + publicKeyHeader + "\", \"otpcode\": \"123456\"}";
+            var invalidPostPayload = "{\"publicKey\": \"" + privateKey + "\", \"otpcode\": \"654321\"}";
+
+            long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            currentTimestamp = (currentTimestamp / 30) * 30; // Round to the nearest 30 seconds
+
+            var payload = new
+            {
+                publicKey = publicKeyHeader,
+                timestamp = currentTimestamp.ToString(),
+                postPayload = invalidPostPayload // Using an invalid payload
+            };
+
+            var payloadJson = JsonConvert.SerializeObject(payload, Formatting.None);
+            var payloadBytes = Encoding.UTF8.GetBytes(payloadJson);
+
+            // Perform the signature calculation using the appropriate private key
+            var signature = SignPayload(privateKey, payloadBytes);
+            var signatureHeader = Convert.ToBase64String(signature);
+
+            var logger = new Mock<ILogger<SignatureVerificationMiddleware>>();
+            var context = new DefaultHttpContext();
+
+            context.Request.Headers["x-public-key"] = publicKeyHeader;
+            context.Request.Headers["x-signature"] = signatureHeader;
+
+            var requestStream = new MemoryStream(Encoding.UTF8.GetBytes(validPostPayload)); // Using a different payload here
+            context.Request.Body = requestStream;
+            context.Request.Method = "POST";
+
+            var next = new RequestDelegate(innerContext => Task.CompletedTask);
+            var middleware = new SignatureVerificationMiddleware(next, logger.Object);
+
+            // Act
+            await middleware.Invoke(context);
+
+            // Assert
+            // Ensure that the middleware returns a 403 status code when the signature is invalid
+            Assert.AreEqual(403, context.Response.StatusCode);
+        }
+
+        // Helper method to sign the payload using the private key
+        private static byte[] SignPayload(string privateKey, byte[] payload)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportFromPem(privateKey);
+
+                return rsa.SignData(payload, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+    }
+}
---
+++ backend/core/src/Core.API/ResponseHandling/SignatureVerificationMiddleware.cs
@@ -0,0 +1,150 @@
+﻿// Copyright 2023 Quantoz Technology B.V. and contributors. Licensed
+// under the Apache License, Version 2.0. See the NOTICE file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+using Core.Domain.Exceptions;
+using Core.Presentation.Models;
+using Newtonsoft.Json;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.API.ResponseHandling
+{
+    public class SignatureVerificationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<SignatureVerificationMiddleware> _logger;
+
+        public SignatureVerificationMiddleware(
+            RequestDelegate next,
+            ILogger<SignatureVerificationMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                // Retrieve headers from the request
+                string? publicKeyHeader = context.Request.Headers["x-public-key"];
+                string? signatureHeader = context.Request.Headers["x-signature"];
+
+                // Retrieve method from the request
+                var method = context.Request.Method;
+
+                // Ensure both headers are present
+                if (string.IsNullOrWhiteSpace(publicKeyHeader) || string.IsNullOrWhiteSpace(signatureHeader))
+                {
+                    _logger.LogError("Missing x-signature header");
+                    var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "x-signature"));
+                    await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                    return;
+                }
+
+                // Get the current Unix UTC timestamp (rounded to 30 seconds)
+                long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                currentTimestamp = (currentTimestamp / 30) * 30; // Round to the nearest 30 seconds
+
+                string? postPayload = string.Empty;
+
+                if (method == "POST")
+                {
+                    using (StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8))
+                    {
+                        postPayload = await reader.ReadToEndAsync();
+                    }
+                }
+
+                string payload = JsonConvert.SerializeObject(new
+                {
+                    publicKey = publicKeyHeader,
+                    timestamp = currentTimestamp.ToString(),
+                    postPayload
+                }, Formatting.None);
+
+                byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+                // Decode the signature header from Base64
+                byte[]? signatureBytes = Convert.FromBase64String(signatureHeader);
+
+                // Verify the signature
+                if (VerifySignature(publicKeyHeader, payloadBytes, signatureBytes))
+                {
+                    await _next(context); // Signature is valid, continue with the request
+                }
+                else
+                {
+                    // Check the previous timestamp (30 seconds ago)
+                    long previousTimestamp = currentTimestamp - 30;
+
+                    string updatedPayload = JsonConvert.SerializeObject(new
+                    {
+                        publicKey = publicKeyHeader,
+                        timestamp = previousTimestamp.ToString(),
+                        postPayload
+                    });
+
+                    payloadBytes = Encoding.UTF8.GetBytes(updatedPayload);
+
+                    // Verify the signature again with the previous timestamp
+                    if (VerifySignature(publicKeyHeader, payloadBytes, signatureBytes))
+                    {
+                        await _next(context); // Signature is valid, continue with the request
+                    }
+                    else
+                    {
+                        _logger.LogError("Invalid signature");
+                        var customErrors = new CustomErrors(new CustomError("Forbidden", "Invalid signature", "x-signature"));
+                        await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unknown exception thrown: {message}", ex.Message);
+                throw new Exception("Unknown exception thrown: {message}", ex);
+            }
+        }
+
+        private static async Task WriteCustomErrors(HttpResponse httpResponse, CustomErrors customErrors, int statusCode)
+        {
+            httpResponse.StatusCode = statusCode;
+            httpResponse.ContentType = "application/json";
+
+            var response = CustomErrorsResponse.FromCustomErrors(customErrors);
+            var json = System.Text.Json.JsonSerializer.Serialize(response);
+            await httpResponse.WriteAsync(json);
+        }
+
+        private static bool VerifySignature(string publicKey, byte[] payload, byte[] signature)
+        {
+            try
+            {
+                using (RSA rsa = RSA.Create())
+                {
+                    // Import the public key (assuming it's in PEM format)
+                    rsa.ImportFromPem(publicKey);
+
+                    // Verify the signature using the SHA256 algorithm and PKCS1 padding
+                    return rsa.VerifyData(payload, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                }
+            }
+            catch (CryptographicException)
+            {
+                // Signature verification failed
+                return false;
+            }
+        }
+    }
+
+    public static class SignatureVerificationMiddlewareExtensions
+    {
+        public static void ConfigureSignatureVerificationMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<SignatureVerificationMiddleware>();
+        }
+    }
+}
