a8de6d43329891cd6e8414090f316797da8fc1be
neha@quantoz <neha@quantoz.com>
2023-11-27 22:00:42 +0100

    Extracted PublicKey and Payload from Header, refactored the code

--- backend/core/src/Core.API/ResponseHandling/SignatureVerificationMiddleware.cs
+++ backend/core/src/Core.API/ResponseHandling/SignatureVerificationMiddleware.cs
@@ -3,12 +3,12 @@
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 using Core.Domain.Exceptions;
-using Core.Domain.SignatureVerification;
 using Core.Presentation.Models;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+using static Core.Domain.Constants;
 
 namespace Core.API.ResponseHandling
 {
@@ -30,14 +30,21 @@ namespace Core.API.ResponseHandling
             try
             {
                 // Retrieve headers from the request
-                string? publicKeyHeader = context.Request.Headers["x-public-key"];
                 string? signatureHeader = context.Request.Headers["x-signature"];
+                string? payloadHeader = context.Request.Headers["x-payload"];
 
                 // Retrieve method from the request
                 var method = context.Request.Method;
 
-                // Ensure both headers are present
-                if (string.IsNullOrWhiteSpace(publicKeyHeader) || string.IsNullOrWhiteSpace(signatureHeader))
+                if (string.IsNullOrWhiteSpace(payloadHeader))
+                {
+                    _logger.LogError("Missing payload header");
+                    var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "x-payload"));
+                    await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(signatureHeader))
                 {
                     _logger.LogError("Missing x-signature header");
                     var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "x-signature"));
@@ -45,86 +52,72 @@ namespace Core.API.ResponseHandling
                     return;
                 }
 
-                // Get the current Unix UTC timestamp (rounded to 30 seconds)
-                long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                byte[] payloadBytes = Encoding.UTF8.GetBytes(payloadHeader);
 
-                string? postPayload = string.Empty;
-                string? payload = null;
+                JObject payloadJson = JObject.Parse(payloadHeader);
 
-                switch (method)
+                string? publicKey = null;
+                string? postPayload = null;
+
+                if (method == "POST" || method == "PUT")
                 {
-                    case "POST":
-                    case "PUT":
-                        {
-                            using (StreamReader reader = new(context.Request.Body, Encoding.UTF8))
-                            {
-                                postPayload = await reader.ReadToEndAsync();
-                            }
-
-                            payload = JsonConvert.SerializeObject(new SignaturePayload
-                            {
-                                PublicKey = publicKeyHeader,
-                                Timestamp = currentTimestamp,
-                                PostPayload = postPayload
-                            }, Formatting.None);
-
-                            break;
-                        }
-
-                    default:
-                        payload = JsonConvert.SerializeObject(new SignaturePayload
-                        {
-                            PublicKey = publicKeyHeader,
-                            Timestamp = currentTimestamp
-                        }, Formatting.None);
-
-                        break;
+                    // Check if the "PostPayload" property is present
+                    if (payloadJson.TryGetValue(SignaturePayload.PostPayload, out var post))
+                    {
+                        postPayload = (string?)post;
+                    }
+                    else
+                    {
+                        _logger.LogError("Missing postPayload header");
+                        var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "postPayload"));
+                        await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                        return;
+                    }
                 }
 
-                byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+                // Check if the "publicKey" property is present
+                if (payloadJson.TryGetValue(SignaturePayload.PublicKey, out var pubKey))
+                {
+                    publicKey = (string?)pubKey;
+                }
+                else
+                {
+                    _logger.LogError("Missing publicKey header");
+                    var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "publicKey"));
+                    await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                    return;
+                }
+
+                // Get the current Unix UTC timestamp (rounded to 30 seconds)
+                long currentTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
 
                 // Decode the signature header from Base64
                 byte[]? signatureBytes = Convert.FromBase64String(signatureHeader);
 
-                // Verify the signature
-                if (VerifySignature(publicKeyHeader, payloadBytes, signatureBytes))
+                long timestamp = 0;
+
+                // Check if the "timestamp" property is present
+                if (payloadJson.TryGetValue(SignaturePayload.Timestamp, out var timestampToken))
                 {
-                    await _next(context); // Signature is valid, continue with the request
+                    // Extract the timestamp value
+                    timestamp = (long)timestampToken;
                 }
                 else
                 {
-                    // Check the previous timestamp (30 seconds ago)
-                    long previousTimestamp = currentTimestamp - 30;
+                    _logger.LogError("Missing timestamp in header");
+                    var customErrors = new CustomErrors(new CustomError("Forbidden", "Missing Header", "timestamp"));
+                    await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
+                    return;
+                }
 
-                    string? updatedPayload = null;
+                bool isCurrentTime = timestamp == currentTimestamp;
 
-                    switch (method)
-                    {
-                        case "POST":
-                        case "PUT":
-                            {
-                                updatedPayload = JsonConvert.SerializeObject(new SignaturePayload
-                                {
-                                    PublicKey = publicKeyHeader,
-                                    Timestamp = previousTimestamp,
-                                    PostPayload = postPayload
-                                }, Formatting.None);
-                                break;
-                            }
+                long allowedDifference = 30; // 30 seconds
+                bool isWithin30Seconds = Math.Abs(currentTimestamp - timestamp) <= allowedDifference;
 
-                        default:
-                            updatedPayload = JsonConvert.SerializeObject(new SignaturePayload
-                            {
-                                PublicKey = publicKeyHeader,
-                                Timestamp = previousTimestamp,
-                            }, Formatting.None);
-                            break;
-                    }
-
-                    payloadBytes = Encoding.UTF8.GetBytes(updatedPayload);
-
-                    // Verify the signature again with the previous timestamp
-                    if (VerifySignature(publicKeyHeader, payloadBytes, signatureBytes))
+                if (isCurrentTime || isWithin30Seconds)
+                {
+                    if (VerifySignature(publicKey!, payloadBytes, signatureBytes))
                     {
                         await _next(context); // Signature is valid, continue with the request
                     }
@@ -134,6 +127,12 @@ namespace Core.API.ResponseHandling
                         var customErrors = new CustomErrors(new CustomError("Forbidden", "Invalid signature", "x-signature"));
                         await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
                     }
+                }
+                else
+                {
+                    _logger.LogError("Invalid signature");
+                    var customErrors = new CustomErrors(new CustomError("Forbidden", "Invalid signature", "x-signature"));
+                    await WriteCustomErrors(context.Response, customErrors, (int)HttpStatusCode.Forbidden);
                 }
             }
             catch (Exception ex)
@@ -163,7 +162,9 @@ namespace Core.API.ResponseHandling
                     rsa.ImportFromPem(publicKey);
 
                     // Verify the signature using the SHA256 algorithm and PKCS1 padding
-                    return rsa.VerifyData(payload, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                    var isValidSignature = rsa.VerifyData(payload, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+                    return isValidSignature;
                 }
             }
             catch (CryptographicException)
