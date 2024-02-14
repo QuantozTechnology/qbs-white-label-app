b6b0fcc4ca79f20e944cf9e8cb57778df66c6245
Arash <arash@igne.nl>
2024-02-14 12:31:39 +0100

    fix useDeviceVerification to run it manually + Handle verifyDevice response

--- mobile/src/utils/hooks/useDeviceVerification.ts
+++ mobile/src/utils/hooks/useDeviceVerification.ts
@@ -7,8 +7,9 @@ import * as forge from "node-forge";
 import * as SecureStore from "expo-secure-store";
 import { verifyDevice } from "../../api/customer/devices";
 import { isAxiosError } from "axios";
+import { isNil } from "lodash";
 
-export function useDeviceVerification() {
+export function useDeviceVerification(shouldVerify: boolean) {
   const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [deviceConflict, setDeviceConflict] = useState(false);
@@ -23,7 +24,7 @@ export function useDeviceVerification() {
   const storeKeys = async (
     pubKey: string,
     privKey: string,
-    otpSeed?: string
+    otpSeed?: string | null
   ) => {
     await SecureStore.setItemAsync("publicKey", pubKey);
     await SecureStore.setItemAsync("privateKey", privKey);
@@ -33,37 +34,49 @@ export function useDeviceVerification() {
   };
 
   useEffect(() => {
-    const setupAndVerifyDeviceSecurity = async () => {
-      setIsLoading(true);
-
-      try {
-        let pubKey = await SecureStore.getItemAsync("publicKey");
-        let privKey = await SecureStore.getItemAsync("privateKey");
-
-        if (!pubKey || !privKey) {
-          const keys = generateKeys();
-          pubKey = keys.pubKey;
-          privKey = keys.privKey;
-
-          await storeKeys(pubKey, privKey);
-        }
-
-        const { data } = await verifyDevice({ publicKey: pubKey });
-
-        await storeKeys(pubKey, privKey, data.value.otpSeed);
-      } catch (e: unknown) {
-        if (isAxiosError(e) && e.response?.status === 409) {
-          setDeviceConflict(true);
-        } else {
-          setError(new Error("Error verifying device: " + e));
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
+    if (!shouldVerify) {
+      return;
+    }
     setupAndVerifyDeviceSecurity();
-  }, []);
+  }, [shouldVerify]);
+
+  const setupAndVerifyDeviceSecurity = async () => {
+    setIsLoading(true);
+
+    try {
+      let pubKey = await SecureStore.getItemAsync("publicKey");
+      let privKey = await SecureStore.getItemAsync("privateKey");
+
+      if (!pubKey || !privKey) {
+        const keys = generateKeys();
+        pubKey = keys.pubKey;
+        privKey = keys.privKey;
+
+        await storeKeys(pubKey, privKey, null);
+      }
+      const verificationResult = await verifyDevice({ publicKey: pubKey });
+      // Handle the case where the API response is not as expected, so we don't run into errors
+      try {
+        const { data } = verificationResult;
+        const otpSeed = isNil(data?.value?.otpSeed)
+          ? null
+          : data?.value?.otpSeed;
+        if (otpSeed) {
+          await storeKeys(pubKey, privKey, otpSeed);
+        }
+      } catch (e) {
+        console.log("error in verifyDevice", e);
+      }
+    } catch (e: unknown) {
+      if (isAxiosError(e) && e.response?.status === 409) {
+        setDeviceConflict(true);
+      } else {
+        setError(new Error("Error verifying device: " + e));
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return {
     error,
