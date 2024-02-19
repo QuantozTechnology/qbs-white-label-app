9e1423befaf4bdc01f62160b53a4800fcb692a59
Raymen Scholten <raymens@users.noreply.github.com>
2024-02-19 13:52:26 +0100

    Add missing export

--- mobile/jest.config.ts
+++ mobile/jest.config.ts
@@ -26,6 +26,10 @@ export default {
     "./src/jest/jest.setup.js",
   ],
 
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+
   testEnvironment: "jsdom",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|axios|@sentry/.*|sentry-expo|@noble/ed25519*)",
